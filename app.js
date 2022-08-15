b38f50236d44e112c790ec3165c222e4502b778c
“matthewhasemeier” <mshweb22@gmail.com>
2022-08-15 13:04:24 -0700

    this is wrong, but getting there

--- app.js
+++ app.js
@@ -3,5 +3,11 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
+let num = 0;
+for (num <=100; num +1;);
+ if  (num % 2 = 0 ())
+ else (num % 2  > 0 (console.log num))
+ else if (num >100, break); 
+
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
