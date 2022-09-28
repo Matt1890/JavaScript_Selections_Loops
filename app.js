d6733a878e51449859c2053553cb7c25a045e6c1
“matthewhasemeier” <mshweb22@gmail.com>
2022-09-27 23:30:45 -0700

    fixed typo causing endless loop

--- app.js
+++ app.js
@@ -3,83 +3,90 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
-for (let num = 0; num <= 100; num ++) {
+for (let num = 0; num <= 100; num++) {
   if (num % 2 == 0) {
-  continue;
-  } else { 
+    continue;
+  } else {
     console.log(`${num} is odd`);
-    }
-  };
+  }
+}
 
-  console.log("EXERCISE 2:\n==========\n");
+console.log("EXERCISE 2:\n==========\n");
 
-  for (let num = 0; num <= 100; num ++) {
-    if (num % 3 == 0 && num % 5 == 0) {
-      console.log(`${num} Fizz Buzz`);
-    } else if (num % 5  == 0) {
-      console.log(`${num} Buzz`);
-    } else if (num % 3 == 0) {
-      console.log(`${num} Fizz`);
-    } else {
-      continue;
-    }
-    };
+for (let num = 0; num <= 100; num++) {
+  if (num % 3 == 0 && num % 5 == 0) {
+    console.log(`${num} Fizz Buzz`);
+  } else if (num % 5 == 0) {
+    console.log(`${num} Buzz`);
+  } else if (num % 3 == 0) {
+   console.log(`${num} Fizz`);
+  }
+}
 
-    console.log("EXERCISE 3:\n==========\n");
-  
-    let whileNum = 0;
-      while (whileNum <= 100 && whileNum % 2 > 0) 
-    {
-      whileNum ++; console.log(`${whileNum} is odd`); 
-      };
+console.log("EXERCISE 3:\n==========\n");
 
-    let doNum = 0;
-      do {
-        doNum ++; console.log(`${doNum} is odd`); 
-        } while (doNum <= 100);
+let whileNum = 0;
+while (whileNum <= 100 && whileNum % 2 > 0) {
+  whileNum++;
+  console.log(`${whileNum} is odd`);
+}
 
-    
-        let fbwNum = 0;
-        while (fbwNum <= 100) {
-          fbwNum ++;
-          if (fbwNum % 3 == 0 && fbwNum % 5 == 0) {
-            console.log(`${fbwNum} Fizz Buzz`);
-          } else if (fbwNum % 5  == 0) {
-            console.log(`${fbwNum} Buzz`);
-          } else if (fbwNum % 3 == 0) {
-            console.log(`${fbwNum} Fizz`);
-          } else {
-            continue;
-          }
-          };
+let doNum = 0;
+do {
+  doNum++;
+  console.log(`${doNum} is odd`);
+} while (doNum <= 100);
 
-          let fbwDoNum = 0;
-         do {
-          fbwDoNum ++;
-          if (fbwDoNum % 3 == 0 && fbwDoNum % 5 == 0) {
-            console.log(`${fbwDoNum} Fizz Buzz`);
-          } else if (fbwDoNum % 5  == 0) {
-            console.log(`${fbwDoNum} Buzz`);
-          } else if (fbwDoNum % 3 == 0) {
-            console.log(`${fbwDoNum} Fizz`);
-          } else {
-            continue;
-          }
-          } while (fbwDoNum <= 100);
+let fbwNum = 0;
+while (fbwNum <= 100) {
+  fbwNum++;
+  if (fbwNum % 3 == 0 && fbwNum % 5 == 0) {
+    console.log(`${fbwNum} Fizz Buzz`);
+  } else if (fbwNum % 5 == 0) {
+    console.log(`${fbwNum} Buzz`);
+  } else if (fbwNum % 3 == 0) {
+    console.log(`${fbwNum} Fizz`);
+  } else {
+    continue;
+  }
+}
 
+let fbwDoNum = 0;
+do {
+  fbwDoNum++;
+  if (fbwDoNum % 3 == 0 && fbwDoNum % 5 == 0) {
+    console.log(`${fbwDoNum} Fizz Buzz`);
+  } else if (fbwDoNum % 5 == 0) {
+    console.log(`${fbwDoNum} Buzz`);
+  } else if (fbwDoNum % 3 == 0) {
+    console.log(`${fbwDoNum} Fizz`);
+  } else {
+    continue;
+  }
+} while (fbwDoNum <= 100);
 
+console.log("EXERCISE 4:\n==========\n");
 
+let value = Math.round(Math.random() * 500);
+// creates a random number between 0 and 500
+let n = Math.round(Math.random() * (500 - 100) + 100);
+// creates a random number between 100 and 500
 
- 
+for (let checkValue = 0; checkValue <= n; checkValue++) {
+  if (checkValue == value) {
+    console.log(`Found value! ${checkValue}`);
+    break;
+  } else if (checkValue == n) {
+    console.log("Did not find value.");
+  }
+}
 
+//  for (let bottlesOnTheWall = 99; bottlesOnTheWall >= 0; bottlesOnTheWall--) {
+//      if (bottlesOnTheWall) {
+//        console.log(`${bottlesOnTheWall} bottles of milk on the wall...`);
+//      } else {
+//        console.log(`No more bottles of milk on the wall!`);
+//      }
+//    }
 
-  //  for (let bottlesOnTheWall = 99; bottlesOnTheWall >= 0; bottlesOnTheWall--) {
-  //      if (bottlesOnTheWall) {
-  //        console.log(`${bottlesOnTheWall} bottles of milk on the wall...`);
-  //      } else {
-  //        console.log(`No more bottles of milk on the wall!`);
-  //      }
-  //    }
-
-// Exercise 2 Section
 
